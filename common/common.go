85383edd22ddb660a849c6063a1fe2974e211c69
Alejandro Galue <agalue@opennms.org>
2020-08-20 10:04:43 -0400

    Simplify ReadInput

--- common/common.go
+++ common/common.go
@@ -26,14 +26,13 @@ func NewTableWriter() *tabwriter.Writer {
 
 // ReadInput reads data from a file specified on the CLI context
 func ReadInput(c *cli.Context, dataIndex int) ([]byte, error) {
-	var data []byte
 	ymlFile := c.String("file")
 	if ymlFile == "" {
 		arg := c.Args().Get(dataIndex)
 		if arg == "" {
 			return nil, fmt.Errorf("Content cannot be empty")
 		}
-		data = []byte(arg)
+		return []byte(arg), nil
 	} else if ymlFile == "-" { // TODO Does this work on Windows ?
 		fi, err := os.Stdin.Stat()
 		if err != nil {
@@ -42,23 +41,7 @@ func ReadInput(c *cli.Context, dataIndex int) ([]byte, error) {
 		if fi.Mode()&os.ModeNamedPipe == 0 {
 			return nil, fmt.Errorf("There is no YAML content on STDIN pipe")
 		}
-		data, _ = ioutil.ReadAll(os.Stdin)
-	} else {
-		if fileExists(ymlFile) {
-			data, _ = ioutil.ReadFile(ymlFile)
-		} else {
-			return nil, fmt.Errorf("YAML file %s doesn't exist", ymlFile)
-		}
+		return ioutil.ReadAll(os.Stdin)
 	}
-	return data, nil
-}
-
-func fileExists(filename string) bool {
-	_, err := os.Stat(filename)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
+	return ioutil.ReadFile(ymlFile)
 }
